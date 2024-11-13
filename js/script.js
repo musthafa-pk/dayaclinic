6e0ce7bcda11d431175c711fd61ec3dbd7fb032b
musthafa <mmusthafa270@gmail.com>
2024-11-13 23:42:05 +0530

    push with data-aos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+
 window.addEventListener('load', function() {
     // Hide the loader
     document.getElementById('loader').style.display = 'none';
     // Show the content
     document.getElementById('content').style.display = 'block';
 });
+
